98b635b393ba2be2010f42375ee602bc2660ad5d
renelopez <rene.lopez@unosquare.com>
2017-04-04 01:20:16 -0500

    Added Routing

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Routes from './routes';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 import './index.css';
+
 
 const MOUNT=document.getElementById('root');
 const renderApp=Comp => ReactDOM.render(Comp,MOUNT);
 
 if(module.hot){
-    module.hot.accept('./App',()=> {
-        const NextApp=require('./App').default;
+    module.hot.accept('./routes',()=> {
+        const NextApp=require('./routes').default;
         renderApp(<NextApp />);
     })
 }
-renderApp(<App />);
+renderApp(<Routes />);
